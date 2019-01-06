b972743c94af162950a4d1dcf52b453818aae275
Akshay Vijay Jain <theakshay.jain@gmail.com>
2019-01-06 14:36:10 +0530

    complete pdf commit 1

---
+++ index.js
@@ -0,0 +1,19 @@
+function printLandscape() {
+  var css = '@page { size: landscape; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+  style.type = 'text/css';
+  style.media = 'print';
+
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  head.appendChild(style);
+
+  window.print();
+
+}
