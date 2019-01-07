e85c4c38b9d7588a36bb38a0f4b87e82419b53b0
Akshay Vijay Jain <theakshay.jain@gmail.com>
2019-01-07 12:30:39 +0530

    code fix for printing portrait

--- index.js
+++ index.js
@@ -17,3 +17,22 @@ function printLandscape() {
   window.print();
 
 }
+
+function printPortrait() {
+  var css = '@page { size: portrait; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+style.type = 'text/css';
+style.media = 'print';
+
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+
+head.appendChild(style);
+
+window.print();
+}
